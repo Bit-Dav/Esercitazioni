b234717711810167e3e880b5c95713dae52b5b3f
Davide <davideaprile2000@gmail.com>
2023-04-22 10:04:13 +0200

    Esercizio polizia

---
+++ esercizio Polizia/DatabasePolizia.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_Polizia
+{
+    internal class DatabasePolizia
+    {
+        public static List<Trasgressore> listaTrasgressori = new List<Trasgressore>();
+
+        public void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("============== DATABASE VERBALI ==============\n");
+            Console.WriteLine("1) Totale dei verbali\n" +
+                "2) Ricerca verbale con codice fiscale\n" +
+                "3) Ricerca trasgressori per citta \n" +
+                "4) Ricerca verbale per punti decurtati maggiori di\n" +
+                "5) Ricerca verbale per importo maggiore di\n" +
+                "6) Calcola saldo punti patente con codice fiscale\n" +
+                "7) Esci\n");
+
+            Console.Write("Inserisci Comando: ");
+            int op = int.Parse(Console.ReadLine());
+
+            if (op == 1)
+            {
+                TotaleVerbali();
+            }
+            else if (op == 2)
+            {
+                RicercaVerbale();
+            }
+            else if (op == 3)
+            {
+                RicercaCitta();
+            }
+            else if (op == 4)
+            {
+                RicercaPerPunti();
+            }
+            else if (op == 5)
+            {
+                RicercaPerImporto();
+            }
+            else if (op == 6)
+            {
+                CalcoloPuntiPatente();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void TotaleVerbali()
+        {
+            Console.Clear();
+            Console.WriteLine("============== TOTALE VERBALI ==============\n\n");
+
+            int totaleVerbali = 0;
+            decimal totaleImportoVerbali = 0;
+            foreach(Trasgressore trasgressore in listaTrasgressori)
+            {
+                foreach(Verbale verbale in trasgressore.ListaVerbali)
+                {
+                    totaleVerbali += 1;
+                    totaleImportoVerbali += verbale.ImportoVerbale;
+                }
+            }
+
+            Console.WriteLine($"L'importo totale per {totaleVerbali} verbali effettuati è di: {totaleImportoVerbali.ToString("C2")}\n");
+
+            Console.WriteLine("Vuoi tornare al menu? \n 1) si \n 2) no\n");
+            Console.Write("Inserisci: ");
+            int op = int.Parse(Console.ReadLine());
+
+            if (op == 1)
+            {
+                Menu();
+            }
+
+
+        }
+
+        private void RicercaVerbale()
+        {
+            Console.Clear();
+            Console.WriteLine("============== RICERCA VERBALI ==============\n\n");
+
+            Console.Write("Inserisci il codice fiscale del trasgressore: ");
+            string cf = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("============== RICERCA VERBALI ==============\n");
+
+            bool trovato = false;
+            foreach (Trasgressore trasgressore in listaTrasgressori)
+            {
+                if (cf == trasgressore.CodiceFiscale)
+                {
+                    trovato = true;
+                    trasgressore.StampaTrasgressore();
+                }
+            }
+
+            if (!trovato)
+                Console.WriteLine("Codice fiscale non presente nel database");
+
+            Console.WriteLine("Vuoi tornare al menu? \n 1) si \n 2) no\n");
+            Console.Write("Inserisci: ");
+            int op = int.Parse(Console.ReadLine());
+
+            if (op == 1)
+            {
+                Menu();
+            }
+        }
+
+        private void RicercaCitta()
+        {
+            Console.Clear();
+            Console.WriteLine("============== RICERCA TRASGRESSORI PER CITTA ==============\n\n");
+
+            Console.Write("Inserisci La citta: ");
+            string citta = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("============== RICERCA TRASGRESSORI PER CITTA ==============\n");
+
+            bool trovato = false;
+            foreach (Trasgressore trasgressore in listaTrasgressori)
+            {
+                foreach(Verbale verbale in trasgressore.ListaVerbali)
+                {
+                    if (citta == verbale.CittaViolazione)
+                    {
+                        trovato = true;
+                        trasgressore.StampaTrasgressore();
+                    }
+                }
+                
+            }
+
+            if (!trovato)
+                Console.WriteLine("Citta non presente nel database");
+
+            Console.WriteLine("Vuoi tornare al menu? \n 1) si \n 2) no\n");
+            Console.Write("Inserisci: ");
+            int op = int.Parse(Console.ReadLine());
+
+            if (op == 1)
+            {
+                Menu();
+            }
+        }
+
+        private void RicercaPerPunti()
+        {
+            Console.Clear();
+            Console.WriteLine("============== RICERCA VERBALI PER PUNTI ==============\n\n");
+
+            Console.Write("Inserisci valore: ");
+            int punti = int.Parse(Console.ReadLine()) ;
+            Console.Clear();
+            Console.WriteLine("============== RICERCA VERBALI PER PUNTI ==============\n");
+
+            bool trovato = false;
+            foreach (Trasgressore trasgressore in listaTrasgressori)
+            {
+                    int totalePunti = 0;
+                    foreach (Verbale verbale in trasgressore.ListaVerbali)
+                    {
+                        foreach (TipoViolazione violazione in verbale.Violazioni)
+                        {
+                            totalePunti += violazione.DecurtamentoPunti;
+                        }
+
+                        if(totalePunti > punti)
+                        {
+                            trovato = true;
+                            trasgressore.StampaTrasgressore();
+                        }
+                    }
+            }
+
+            if (!trovato)
+                Console.WriteLine("Non presente");
+
+            Console.WriteLine("Vuoi tornare al menu? \n 1) si \n 2) no\n");
+            Console.Write("Inserisci: ");
+            int op = int.Parse(Console.ReadLine());
+
+            if (op == 1)
+            {
+                Menu();
+            }
+        }
+
+        private void RicercaPerImporto()
+        {
+            Console.Clear();
+            Console.WriteLine("============== RICERCA VERBALI PER IMPORTO ==============\n\n");
+
+            Console.Write("Inserisci importo: ");
+            decimal importo = Convert.ToDecimal(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("============== RICERCA VERBALI PER IMPORTO ==============\n");
+
+            bool trovato = false;
+            foreach (Trasgressore trasgressore in listaTrasgressori)
+            {
+                foreach (Verbale verbale in trasgressore.ListaVerbali)
+                {
+                    if (verbale.ImportoVerbale > importo)
+                    {
+                        trovato = true;
+                        trasgressore.StampaTrasgressore();
+                    }
+                }
+            }
+
+            if (!trovato)
+                Console.WriteLine("Non presente");
+
+            Console.WriteLine("Vuoi tornare al menu? \n 1) si \n 2) no\n");
+            Console.Write("Inserisci: ");
+            int op = int.Parse(Console.ReadLine());
+
+            if (op == 1)
+            {
+                Menu();
+            }
+        }
+
+        private void CalcoloPuntiPatente()
+        {
+            Console.Clear();
+            Console.WriteLine("============== CALCOLO PUNTI PATENTE ==============\n\n");
+
+            Console.Write("Inserisci il codice fiscale del trasgressore: ");
+            string cf = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("============== CALCOLO PUNTI PATENTE ==============\n");
+
+            decimal totalePuntiPatente = 20;
+            bool trovato = false;
+            foreach (Trasgressore trasgressore in listaTrasgressori)
+            {
+                if(trasgressore.CodiceFiscale == cf)
+                {
+                    trovato = true;
+                    foreach (Verbale verbale in trasgressore.ListaVerbali)
+                    {
+                        foreach (TipoViolazione violazione in verbale.Violazioni)
+                        {
+                            totalePuntiPatente -= violazione.DecurtamentoPunti;
+                        }
+                    }
+
+                    Console.WriteLine($"{trasgressore.Nome} {trasgressore.Cognome} ha {totalePuntiPatente} punti sulla patente");
+                }
+            }
+
+            if (!trovato)
+                Console.WriteLine("\nCodice fiscale non presente nel database");
+
+            Console.WriteLine("\n\nVuoi tornare al menu? \n 1) si \n 2) no\n");
+            Console.Write("Inserisci: ");
+            int op = int.Parse(Console.ReadLine());
+
+            if (op == 1)
+            {
+                Menu();
+            }
+        }
+    }
+
+    public class Trasgressore
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Indirizzo { get; set; }
+        public string Citta { get; set; }
+        public int CAP { get; set; }
+        public string CodiceFiscale { get; set; }
+        public List<Verbale> ListaVerbali { get; set; } = new List<Verbale>();
+
+        public Trasgressore(int id, string nome, string cognome, string indirizzo, string citta, int cap, string cf, List<Verbale> verbali)
+        {
+            Id = id;
+            Nome = nome;
+            Cognome = cognome;
+            Indirizzo = indirizzo;
+            Citta = citta;
+            CAP = cap;
+            CodiceFiscale = cf;
+            ListaVerbali = verbali;
+        }
+
+        public void StampaTrasgressore()
+        {
+            
+                    decimal totaleImportoVerbali = 0;
+                    int totalePunti = 0;
+                    foreach (Verbale verbale in ListaVerbali)
+                    {
+
+                        Console.WriteLine($"Nome: {Nome} Cognome: {Cognome}\n" +
+                            $"Data violazione: {verbale.DataViolazione.ToShortDateString()}\n" +
+                            $"Indirizzo violazione: {verbale.IndizizzoViolazione}\n" +
+                            $"Citta violazione: {verbale.CittaViolazione}\n" +
+                            $"Agente verbalizzante: {verbale.AgenteVerbalizzante}\n" +
+                            $"Trascrizione verbale: {verbale.DataTrascrizioneVerbale.ToShortDateString()}\n" +
+                            $"Importo verbale: {verbale.ImportoVerbale.ToString("C2")}\n" +
+                            $"Targa veicolo verbale: {verbale.Veicolo.Targa}\n");
+
+                        int i = 0;
+                        foreach (TipoViolazione violazione in verbale.Violazioni)
+                        {
+                            i++;
+                            Console.WriteLine($"Violazione {i}: {violazione.Descrizione} || Punti decurtati per violazione: {violazione.DecurtamentoPunti}");
+                            totalePunti += violazione.DecurtamentoPunti;
+                        }
+
+                             Console.WriteLine("\n------------------------------------------------------------------------------\n");
+                             totaleImportoVerbali += verbale.ImportoVerbale;
+                    }
+
+                    Console.WriteLine($"I punti totali decurtati al trasgressore sono di: {totalePunti}");
+                    Console.WriteLine($"L'importo totale per {ListaVerbali.Count} verbali effettuati è di: {totaleImportoVerbali.ToString("C2")}");
+                    Console.WriteLine("\n------------------------------------------------------------------------------\n");
+
+        }
+
+    }
+
+    public class TipoViolazione
+    {
+        public int IdViolazione { get; set; }
+        public string Descrizione { get; set; }
+        public int DecurtamentoPunti { get; set; }
+
+        public TipoViolazione(int idViolazione, string descrizione, int decurtamentoPunti)
+        {
+            IdViolazione = idViolazione;
+            Descrizione = descrizione;
+            DecurtamentoPunti = decurtamentoPunti;
+        }
+    }
+
+    public class Verbale
+    {
+        public int IdVerbale { get; set; }
+        public DateTime DataViolazione { get; set; }
+        public string IndizizzoViolazione { get; set; }
+        public string CittaViolazione { get; set; }
+        public string AgenteVerbalizzante { get; set; }
+        public DateTime DataTrascrizioneVerbale { get; set; }
+        public decimal ImportoVerbale { get; set; }
+        public List<TipoViolazione> Violazioni { get; set; } = new List<TipoViolazione>();
+        public Veicolo Veicolo { get; set; }
+
+        public Verbale(int idVerbale, DateTime dataViolazione, string indizizzoViolazione, string cittaViolazione, string agenteVerbalizzante, DateTime dataTrascrizioneVerbale, decimal importoVerbale, Veicolo veicolo, List<TipoViolazione> violazioni)
+        {
+            IdVerbale = idVerbale;
+            DataViolazione = dataViolazione;
+            IndizizzoViolazione = indizizzoViolazione;
+            CittaViolazione = cittaViolazione;
+            AgenteVerbalizzante = agenteVerbalizzante;
+            DataTrascrizioneVerbale = dataTrascrizioneVerbale;
+            ImportoVerbale = importoVerbale;
+            Veicolo = veicolo;
+            Violazioni = violazioni;
+        }
+    }
+
+    public class Veicolo
+    {
+        public string Targa { get; set; }
+        public string NrTelaio { get; set; }
+        public string MarcaVeicolo { get; set; }
+        public string TipoVeicolo { get; set; }
+
+        public Veicolo(string targa, string nrTelaio, string marcaVeicolo, string tipoVeicolo)
+        {
+            Targa = targa;
+            NrTelaio = nrTelaio;
+            MarcaVeicolo = marcaVeicolo;
+            TipoVeicolo = tipoVeicolo;
+        }
+    }
+
+}
---
+++ esercizio Polizia/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_Polizia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            TipoViolazione violazione = new TipoViolazione(1, "Guida Senza patente", 3);
+            TipoViolazione violazione1 = new TipoViolazione(2, "Violazione divieto di sosta", 2);
+            TipoViolazione violazione2 = new TipoViolazione(3, "Eccesso di velocità", 3);
+            TipoViolazione violazione3 = new TipoViolazione(4, "Guida in stato di ebbrezza", 5);
+            TipoViolazione violazione4 = new TipoViolazione(5, "Uso di cellulare durante la guida", 3);
+            TipoViolazione violazione5 = new TipoViolazione(6, "Guida senza cinture di sicurezza", 3);
+
+            Veicolo veicolo = new Veicolo("AA382EO","ERREBVE", "FIAT Panda", "Autoveicolo");
+            Veicolo veicolo1 = new Veicolo("GG333QQ","BBE3ER3", "Alfa Romeo Giulia", "Autoveicolo");
+            Veicolo veicolo2 = new Veicolo("BE838ER","33333A", "Maserati Levante", "Autoveicolo");
+            Veicolo veicolo3 = new Veicolo("FH332LP","FFFFFF", "Audi A3", "Autoveicolo");
+
+            Verbale verbale = new Verbale(1, new DateTime(2023,04,21), "Via dell'Unità", "Napoli", "Maresciallo Mimmo", new DateTime(2023, 04, 21), 1000m, veicolo3, new List<TipoViolazione>() { violazione, violazione2 });
+            Verbale verbale1 = new Verbale(2, new DateTime(2023,04,21), "Via Sant Antonio", "Milano", "Paolo Crisali", new DateTime(2023, 04, 19), 200m, veicolo, new List<TipoViolazione>() { violazione1 });
+            Verbale verbale2 = new Verbale(3, new DateTime(2023,02,11), "Via Martiri", "Roma", "Federico Stoppa", new DateTime(2023, 02, 11), 5000m, veicolo2, new List<TipoViolazione>() { violazione, violazione2, violazione4, violazione5, violazione3 });
+            Verbale verbale3 = new Verbale(4, new DateTime(2023,01,10), "Viale San Gerardo", "Napoli", "Maresciallo Mimmo", new DateTime(2023, 01, 10), 2000m, veicolo3, new List<TipoViolazione>() { violazione4, violazione5, violazione2 });
+            Verbale verbale4 = new Verbale(5, new DateTime(2023,02,8), "Viale Monte", "Roma", "Pietro Somma", new DateTime(2023, 02, 8), 500m, veicolo1, new List<TipoViolazione>() { violazione2 });
+
+            Trasgressore trasgressore = new Trasgressore(1, "Lorenzo", "Pagani", "Via Laggiu", "Napoli", 100234, "LRPG", new List<Verbale>() { verbale, verbale3 });
+            Trasgressore trasgressore1 = new Trasgressore(2, "Giancarlo", "Monti", "Via DaQui", "Milano", 100234, "GCMN", new List<Verbale>() { verbale1 });
+            Trasgressore trasgressore2 = new Trasgressore(3, "Karim", "Musa", "Via Loreto", "Roma", 100234, "KMMU", new List<Verbale>() { verbale4 });
+            Trasgressore trasgressore3 = new Trasgressore(4, "Mario", "Rossi", "Via Carafa", "Bologna", 100234, "MRRS", new List<Verbale>() { verbale2 });
+
+            DatabasePolizia.listaTrasgressori.Add(trasgressore);
+            DatabasePolizia.listaTrasgressori.Add(trasgressore1);
+            DatabasePolizia.listaTrasgressori.Add(trasgressore2);
+            DatabasePolizia.listaTrasgressori.Add(trasgressore3);
+
+            DatabasePolizia db = new DatabasePolizia();
+            db.Menu();
+        }
+    }
+}
